58a10e3952f5230a9d4ad8eb63a70488adee0250
Heather Christman <efl7a@yahoo.de>
2018-04-24 14:20:20 -0400

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,14 +3,9 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def inventory
+  def data
     product = Product.find(params[:id])
-    render plain: product.inventory > 0 ? true : false
-  end
-
-  def description
-    product = Product.find(params[:id])
-    render plain: product.description
+    render json: ProductSerializer.serialize(product)
   end
 
   def new
@@ -20,6 +15,20 @@ class ProductsController < ApplicationController
   def create
     Product.create(product_params)
     redirect_to products_path
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def description
+    product = Product.find(params[:id])
+    render plain: product.description
+  end
+
+  def inventory
+    product = Product.find(params[:id])
+    render plain: product.inventory > 0 ? "true":"false"
   end
 
   private
