5da4cb0c0977d50e5e499180ed00bca1a1c26e94
Alex Segers <a.segers.dev@gmail.com>
2018-08-28 18:23:00 +0000

    Done.

--- app/services/product_serializer.rb
+++ app/services/product_serializer.rb
@@ -2,11 +2,12 @@ class ProductSerializer
 
   def self.serialize(product)
     description = product.description.nil? ? "" : product.description
+    inventory = product.inventory.nil? ? "0" : product.inventory.to_s
     serialized_product = '{'
     serialized_product += '"id": "' + product.id.to_s + '", '
     serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"price": ' + product.price.to_s + ', '
-    serialized_product += '"inventory": ' + product.inventory.to_s + ', '
+    serialized_product += '"inventory": ' + inventory + ', '
     serialized_product += '"description": "' + description + '"}'
   end
 
